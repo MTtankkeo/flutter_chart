043b93e09d22521b451cfc7ebc90319cebfb926b
Dev Ttangkong <ttankkeo112@gmail.com>
2025-02-04 22:15:00 +0900

    Update LICENSE and flutter_chart.dart

--- lib/flutter_chart.dart
+++ lib/flutter_chart.dart
@@ -1,5 +1,8 @@
 library flutter_chart;
 
+export 'widgets/ChartStyle.dart';
 export 'widgets/DrivenChart.dart';
 export 'widgets/ColumnChart.dart';
+
+export 'components/ChartLabeledData.dart';
 export 'components/types.dart';
