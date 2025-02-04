1a3dd8cb06efe8c99ae8c25faddfbf7956abf9ab
Dev Ttangkong <ttankkeo112@gmail.com>
2025-02-05 04:23:29 +0900

    commit for backup about flutter_chart.dart

--- lib/flutter_chart.dart
+++ lib/flutter_chart.dart
@@ -3,6 +3,10 @@ library flutter_chartx;
 export 'widgets/ChartStyle.dart';
 export 'widgets/DrivenChart.dart';
 export 'widgets/ColumnChart.dart';
+export 'widgets/ChartContext.dart';
 
-export 'components/ChartLabeledData.dart';
 export 'components/types.dart';
+export 'components/ChartAnimation.dart';
+export 'components/ChartController.dart';
+export 'components/ChartState.dart';
+export 'components/ChartLabeledData.dart';
